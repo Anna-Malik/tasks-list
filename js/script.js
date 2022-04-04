2872fa6f3002c1e38960cd2fdd97d1732ff2e1e8
Anna Malik <malik.anna1992@gmail.com>
2022-04-04 12:04:57 +0200

    adding new tasks

--- js/script.js
+++ js/script.js
@@ -18,7 +18,9 @@ const render = () => {
     for (const task of tasks) {
 
         htmlString += `
-        <li>
+        <li 
+        ${task.done ? "style=\"text-decoration: line-through\"" : ""}
+        >
         ${task.content}
         </li>
         `;
@@ -26,12 +28,36 @@ const render = () => {
 
     document.querySelector(".js-tasks").innerHTML = htmlString;
 }
+
+const addNewTask = (newTaskContent) => {
+
+    tasks.push({
+        content: newTaskContent,
+    });
+
+    render();
+}
+
+const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+    
+    if (newTaskContent === "") {
+        return;
+    }
+addNewTask(newTaskContent);
+ 
+}
 const init = () => {
     render();
 
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", (onFormSubmit)
+    )
 
 };
-
 
 init();
 

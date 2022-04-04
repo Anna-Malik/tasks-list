1946c1ed4b54850743f7f725e15ba5b646baf9c0
Anna Malik <malik.anna1992@gmail.com>
2022-04-04 12:25:09 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -21,12 +21,22 @@ const render = () => {
         <li 
         ${task.done ? "style=\"text-decoration: line-through\"" : ""}
         >
+        <button class="js-remove">Usuń zadanie</button>
         ${task.content}
         </li>
         `;
     }
 
     document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButton, index) => {
+        removeButton.addEventListener("click", () => {
+            removeTask(index);
+            
+        });
+    });
 }
 
 const addNewTask = (newTaskContent) => {
@@ -36,6 +46,11 @@ const addNewTask = (newTaskContent) => {
     });
 
     render();
+}
+const removeTask = (taskIndex) => {
+
+tasks.splice(taskIndex, 1);
+            render();
 }
 
 const onFormSubmit = (event) => {
